79d54ea3bb46c141c96c6ea29f8d05daa5560141
zoidberg <dvzlenko@gmail.com>
2020-09-18 17:40:12 +0300

    some endpoint comments, not more

--- stm32f103/STM32_USB-FS-Device_Driver/inc/otgd_fs_dev.h
+++ stm32f103/STM32_USB-FS-Device_Driver/inc/otgd_fs_dev.h
@@ -49,10 +49,10 @@
 #define EP0_OUT                    0x00  
 #define EP0_IN                     0x80  
 #define EP1_OUT                    0x01  
-#define EP1_IN                     0x81  
+#define EP1_IN                     0x81		// in ANDROID: IF = device.getInterface(1); --> EP = IF.getEndpoint(1); 
 #define EP2_OUT                    0x02  
-#define EP2_IN                     0x82  
-#define EP3_OUT                    0x03  
+#define EP2_IN                     0x82		// in ANDROID: IF = device.getInterface(0); --> EP = IF.getEndpoint(0);
+#define EP3_OUT                    0x03		// in ANDROID: IF = device.getInterface(1); --> EP = IF.getEndpoint(0); 
 #define EP3_IN                     0x83  
 
 
